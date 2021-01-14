fbd9473008f6746a71c0b7bfc9951a9ffb42230e
milovanovic <barun@eecs.berkeley.edu>
2021-01-14 11:04:07 +0100

    Add minimum circuit

---
+++ src/main/scala/MinimumCircuit.scala
@@ -0,0 +1,68 @@
+package cfar
+
+import chisel3._
+import chisel3.experimental._
+import chisel3.util._
+
+import dsptools._
+import dsptools.numbers._
+
+import chisel3.stage.{ChiselGeneratorAnnotation, ChiselStage}
+
+object FindMin
+{
+  /** Find minimum inside input vector data
+    *
+    * @param in1 input1 data sequence (input to the MinimumCircuit)
+    * @param in2 input2 data sequence (min seq from the upper layer)
+    * @param n size of the input sequence
+    * @param size if run time is not active have the same value as parameter n
+    *
+    * @example {{{
+    * val findMin = FindMin(in1, in2, 16, 16.U)
+    * }}}
+    */
+  def apply[T <: Data: Real](in1: Seq[T], in2: Seq[T], n: Int, size: Option[UInt]): T = {
+    require(n > 0, "Input vector size should be positive")
+    if (n != 1) {
+      val sel_n = n.U === size.getOrElse(0.U) // if run time is off provide that sel signals are always false
+      val input_to_min = in1.zip(in2).map { case (a,b) => Mux(sel_n, a, b) }
+      val mins = input_to_min.grouped(2).map(pair => Mux(pair(0) < pair(1), pair(0), pair(1)))
+      apply(in1.take(n/2), mins.toSeq, n/2, size)
+    } else {
+      in2.head
+    }
+  }
+}
+
+class MinimumCircuit[T <: Data: Real](val protoIn: T, val n: Int = 16, val runTime: Boolean = true) extends Module {
+  require(n > 0 && isPow2(n), "Size of the input vector must be a power of 2 and it should be positive")
+  
+  val io = IO(new Bundle {
+    val in = Input(Vec(n, protoIn))
+    val inSize = if (runTime) Some(Input(UInt(log2Up(n+1).W))) else None
+    val out = Output(protoIn)
+  })
+  
+  val size = io.inSize.getOrElse(n)
+  // val mins_wire = Wire(Vec(n/2, protoIn))
+  // first layer should split input vector to pairs, compare and pass min to the FindMin object
+  val mins = io.in.grouped(2).toSeq.map(pair => Mux(pair(0) < pair(1), pair(0), pair(1)))
+
+  //mins_wire.zip(mins).map { case (a,b) => a := b }
+  val minFinal = FindMin(io.in.take(n/2), mins, n/2, io.inSize)
+  io.out := minFinal
+}
+
+
+object MinimumCircuitApp extends App
+{
+  //chisel3.Driver.execute(args,()=>new MinimumCircuit(FixedPoint(16.W, 14.BP))) // replace with ChiselStage
+  chisel3.Driver.execute(args,()=>new MinimumCircuit(FixedPoint(16.W, 14.BP), runTime = false)) // replace with ChiselStage
+}
+
+
+
+
+
+
---
+++ src/test/scala/MinimumCircuitSpec.scala
@@ -0,0 +1,98 @@
+package cfar
+
+import chisel3._
+import chisel3.experimental._
+import chisel3.util.log2Up
+
+import dsptools._
+import dsptools.numbers._
+
+import org.scalatest.{FlatSpec, Matchers}
+import scala.util.Random
+import scala.math.pow
+
+class MinimumCircuitTester[T <: Data: Real](dut: MinimumCircuit[T], tol: Int) extends DspTester(dut) {
+  
+  if (dut.runTime) {
+    val pow2Coll = (1 to log2Up(dut.n)).map(p => pow(2, p).toInt)
+    var testSignal = dut.protoIn match {
+      case f: FixedPoint => Seq.fill(dut.n)((Random.nextDouble() * 2 - 1) * ((1 << dut.protoIn.getWidth - f.binaryPoint.get - 1)))
+      //case f: FixedPoint => (0 until maxDepth * 2).map(c => c.toDouble) // Seq.fill(maxDepth * 2)((Random.nextDouble() * 2 - 1) * ((1 << proto.getWidth - f.binaryPoint.get - 1)))
+      case s: SInt => Seq.fill(dut.n)((Random.nextInt((1 << (dut.protoIn.getWidth - 1)) * 2) - (1 << (dut.protoIn.getWidth - 1))).toDouble)
+      case u: UInt => Seq.fill(dut.n)(Random.nextInt(1 << (dut.protoIn.getWidth)).toDouble)
+      case d: DspReal => Seq.fill(dut.n)((Random.nextInt(Double.MaxValue.toInt) - Double.MaxValue.toInt).toDouble)
+    }
+    for (size <- pow2Coll) {
+      var min = testSignal.take(size).min
+      // define all inputs even though some of them are not consider for min calculation
+      testSignal.zipWithIndex.foreach { case (sample, index) => poke(dut.io.in(index), sample)}
+      poke(dut.io.inSize.get, size)
+      dut.protoIn match {
+        case dspR: DspReal => realTolDecPts.withValue(tol) { expect(dut.io.out, min) }
+        case _ => fixTolLSBs.withValue(tol) { expect(dut.io.out, min) }
+      }
+      step(2)
+    }
+  }
+  else {
+    var testSignal = dut.protoIn match {
+      case f: FixedPoint => Seq.fill(dut.n)((Random.nextDouble() * 2 - 1) * ((1 << dut.protoIn.getWidth - f.binaryPoint.get - 1)))
+      //case f: FixedPoint => (0 until maxDepth * 2).map(c => c.toDouble) // Seq.fill(maxDepth * 2)((Random.nextDouble() * 2 - 1) * ((1 << proto.getWidth - f.binaryPoint.get - 1)))
+      case s: SInt => Seq.fill(dut.n)((Random.nextInt((1 << (dut.protoIn.getWidth - 1)) * 2) - (1 << (dut.protoIn.getWidth - 1))).toDouble)
+      case u: UInt => Seq.fill(dut.n)(Random.nextInt(1 << (dut.protoIn.getWidth)).toDouble)
+      case d: DspReal => Seq.fill(dut.n)((Random.nextInt(Double.MaxValue.toInt) - Double.MaxValue.toInt).toDouble)
+    }
+    var min = testSignal.min
+    
+    testSignal.zipWithIndex.foreach { case (sample, index) => poke(dut.io.in(index), sample)}
+    dut.protoIn match {
+      case dspR: DspReal => realTolDecPts.withValue(tol) { expect(dut.io.out, min) }
+      case _ => fixTolLSBs.withValue(tol) { expect(dut.io.out, min) }
+    }
+    step(4)
+  }
+}
+
+object MinimumCircuitTester {
+  def apply[T <: Data : Real](protoIn: T, n: Int = 16 , runTime: Boolean = false, tol: Int = 1): Boolean = {
+    chisel3.iotesters.Driver.execute(Array("-tbn", "verilator"),
+      () => new MinimumCircuit(protoIn, n, runTime)) { c =>
+      new MinimumCircuitTester(c, tol)
+    }
+  }
+}
+
+class MinimumCircuitSpec extends FlatSpec with Matchers {
+  
+  // FixedPoint
+  for (size <- Seq(2, 4, 8, 16, 32)) {
+    it should s"work with input size vector equal to $size and FixedPoint data input" in {
+      MinimumCircuitTester(FixedPoint(16.W, 5.BP), tol = 2) should be (true)
+    }
+  }
+  
+  for (size <- Seq(2, 8, 16, 32, 64)) {
+    it should s"work with input size vector equal to $size, enabled run time configurable input size and FixedPoint data input" in {
+      MinimumCircuitTester(FixedPoint(16.W, 5.BP), runTime = true, tol = 2) should be (true)
+    }
+  }
+  
+  // SInt
+  for (size <- Seq(2, 8, 16, 32, 64)) {
+    it should s"work with input size vector equal to $size, enabled run time configurable input size and SInt data input" in {
+      MinimumCircuitTester(SInt(16.W), runTime = true, tol = 0) should be (true)
+    }
+  }
+  // UInt
+  for (size <- Seq(2, 8, 16, 32, 64)) {
+    it should s"work with input size vector equal to $size, enabled run time configurable input size and UInt data input" in {
+      MinimumCircuitTester(UInt(16.W), runTime = true, tol = 0) should be (true)
+    }
+  }
+  // DspReal
+  for (size <- Seq(2, 8, 16, 32, 64)) {
+    it should s"work with input size vector equal to $size, enabled run time configurable input size and DspReal data input" in {
+      MinimumCircuitTester(DspReal(), runTime = true, tol = 12) should be (true)
+    }
+  }
+}
