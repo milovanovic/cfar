c2dd7a7abeb33e98f828b0d4274fc4735faed0e1
milovanovic <barun@eecs.berkeley.edu>
2021-07-08 20:18:14 +0200

    A lot of rework
    - Changing streaming nodes
    - Making registers optional
    - Deleting detectPeaks list

--- src/main/scala/CFARDspBlock.scala
+++ src/main/scala/CFARDspBlock.scala
@@ -13,36 +13,82 @@ import freechips.rocketchip.config._
 import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.regmapper._
 
-abstract class CFARBlock [T <: Data : Real: BinaryRepresentation, D, U, E, O, B <: Data] (params: CFARParams[T], beatBytes: Int) extends LazyModule()(Parameters.empty) with DspBlock[D, U, E, O, B] with HasCSR {
 
-  val streamNode = AXI4StreamIdentityNode()
+trait AXI4CFARStandaloneBlock extends AXI4CFARBlock[FixedPoint] {
+  def standaloneParams = AXI4BundleParameters(addrBits = 32, dataBits = 32, idBits = 1)
+  val ioMem = mem.map { m => {
+    val ioMemNode = BundleBridgeSource(() => AXI4Bundle(standaloneParams))
+
+    m :=
+      BundleBridgeToAXI4(AXI4MasterPortParameters(Seq(AXI4MasterParameters("bundleBridgeToAXI4")))) :=
+      ioMemNode
+
+    val ioMem = InModuleBody { ioMemNode.makeIO() }
+    ioMem
+  }}
+  
+  val ioInNode = BundleBridgeSource(() => new AXI4StreamBundle(AXI4StreamBundleParameters(n = 2)))
+  val ioOutNode = BundleBridgeSink[AXI4StreamBundle]()
+
+  ioOutNode :=
+    AXI4StreamToBundleBridge(AXI4StreamSlaveParameters()) :=
+    streamNode :=
+    BundleBridgeToAXI4Stream(AXI4StreamMasterParameters(n = 2)) :=
+    ioInNode
+
+  val in = InModuleBody { ioInNode.makeIO() }
+  val out = InModuleBody { ioOutNode.makeIO() }
+}
+
+abstract class CFARBlock [T <: Data : Real: BinaryRepresentation, D, U, E, O, B <: Data] (params: CFARParams[T], beatBytes: Int) extends LazyModule()(Parameters.empty) with DspBlock[D, U, E, O, B] with HasCSR {
+  
+  //previous version
+  //val streamNode = AXI4StreamIdentityNode()
+  
+  val masterParams = AXI4StreamMasterParameters(
+    name = "AXI4 Stream xWrDataPreProc",
+    n = 6, // 4*8 -> 32 bits -> maybe can be parametrized from the top
+    numMasters = 1
+  )
+  val slaveParams = AXI4StreamSlaveParameters()
+  val slaveNode  = AXI4StreamSlaveNode(slaveParams)
+  val masterNode = AXI4StreamMasterNode(masterParams)
+  
+  val streamNode = NodeHandle(slaveNode, masterNode)
 
   lazy val module = new LazyModuleImp(this) {
-    val (in, _)  = streamNode.in(0)
-    val (out, _) = streamNode.out(0)
+    val (in, edgeIn) = slaveNode.in.head
+    val (out, edgeOut) = masterNode.out.head
 
     val cfar = Module(new CFARCore(params))
 
     // control registers
     val thresholdScalerWidth = params.protoScaler.getWidth
     val divSumWidth = log2Ceil(log2Ceil(params.leadLaggWindowSize + 1))
+    
     val fftWin = RegInit(params.fftSize.U(log2Ceil(params.fftSize + 1).W))
     val thresholdScaler = RegInit(0.U(thresholdScalerWidth.W))
-    val logOrLinearMode = RegInit(false.B)
-    val divSum = RegInit(0.U(divSumWidth.W))
-    
     val peakGrouping = RegInit(false.B)
-    val cfarAlgorithm = RegInit(0.U(1.W))
     val cfarMode = RegInit(0.U(2.W))
     val windowCells = RegInit(params.leadLaggWindowSize.U(log2Ceil(params.leadLaggWindowSize + 1).W))
     val guardCells = RegInit(params.guardWindowSize.U(log2Ceil(params.guardWindowSize + 1).W))
-    // default take middle value
-    val indexLead = RegInit((params.leadLaggWindowSize/2).U(log2Ceil(params.leadLaggWindowSize + 1).W))
-    val indexLagg = RegInit((params.leadLaggWindowSize/2).U(log2Ceil(params.leadLaggWindowSize + 1).W))
-    // sub window size - used only for CASH algorithm
-    val subWindowSize = RegInit(params.leadLaggWindowSize.U(log2Ceil(params.leadLaggWindowSize + 1).W))
-    //val detectedPeaksList = RegInit(0.U(params.fftSize.W)) // it will use a lot of registers
+
     val detectedPeaksListTmp = RegInit(VecInit(Seq.fill(params.fftSize)(false.B)))
+    
+    // common fields for all CFAR types
+    var commonFields = Seq(
+      RegField(log2Ceil(params.fftSize + 1), fftWin,
+        RegFieldDesc(name = "fftWin", desc = "Determines fft size")),
+      RegField(thresholdScalerWidth, thresholdScaler,
+        RegFieldDesc(name = "thresholdScaler", desc = "Threshold scaler")),
+      RegField(1, peakGrouping,
+        RegFieldDesc(name = "peakGrouping", desc = "Use peak grouping logic for peak detection")),
+      RegField(2, cfarMode,
+        RegFieldDesc(name = "cfarMode", desc = "Defines cfar mode CA (cell averaging), GO (greatest of) or SO (smallest of) or CASH (when cash is included)")),
+      RegField(log2Ceil(params.leadLaggWindowSize + 1), windowCells,
+        RegFieldDesc(name = "windowCells", desc = "Defines size of the leading and lagging window")),
+      RegField(log2Ceil(params.guardWindowSize + 1), guardCells,
+        RegFieldDesc(name = "guardCells", desc = "Defines size of the guard window")))
 
     // connect input
     cfar.io.in.valid := in.valid
@@ -50,18 +96,34 @@ abstract class CFARBlock [T <: Data : Real: BinaryRepresentation, D, U, E, O, B 
     in.ready := cfar.io.in.ready
     cfar.io.fftWin := fftWin
     cfar.io.thresholdScaler := thresholdScaler.asTypeOf(params.protoScaler)
-    cfar.io.logOrLinearMode := logOrLinearMode
     
+    if (params.logOrLinReg) {
+      val logOrLinearMode = RegInit(false.B)
+      logOrLinearMode.suggestName("logOrLinearMode")
+      cfar.io.logOrLinearMode.get := logOrLinearMode
+      commonFields = commonFields :+ RegField(1, logOrLinearMode,
+        RegFieldDesc(name = "logOrLinearMode", desc = "Denotes whether input data is in log or in linear mode"))
+    }
     if (params.CFARAlgorithm != GOSCFARType) {
+      val divSum = RegInit(0.U(divSumWidth.W))
       cfar.io.divSum.get := divSum
+      commonFields = commonFields :+ RegField(divSumWidth, divSum,
+        RegFieldDesc(name = "divSum", desc = "Denotes div factor of the sliding sum when CACFAR algorithm is used"))
     }
     if (params.includeCASH == true) {
+      val subWindowSize = RegInit(params.leadLaggWindowSize.U(log2Ceil(params.leadLaggWindowSize + 1).W))
+      subWindowSize.suggestName("subWindowSize")
+      commonFields = commonFields :+ RegField(log2Ceil(params.leadLaggWindowSize + 1) , subWindowSize,
+        RegFieldDesc(name = "subWindowSize", desc = "Defines sub-window size used in CASH algorithm"))
       cfar.io.subCells.get := subWindowSize
     }
-
     cfar.io.peakGrouping := peakGrouping
+
     if (params.CFARAlgorithm == GOSCACFARType) {
+      val cfarAlgorithm = RegInit(0.U(1.W))
       cfar.io.cfarAlgorithm.get := cfarAlgorithm
+      commonFields = commonFields :+ RegField(1, cfarAlgorithm,
+        RegFieldDesc(name = "cfarAlgorithm", desc = "Defines cfar algorithm, used only if parameter CFARAlgorithm is set to GOSCACFARType"))
     }
     cfar.io.cfarMode    := cfarMode
     cfar.io.windowCells := windowCells
@@ -69,22 +131,20 @@ abstract class CFARBlock [T <: Data : Real: BinaryRepresentation, D, U, E, O, B 
     cfar.io.lastIn      := in.bits.last
     
     if (params.CFARAlgorithm != CACFARType) {
+      val indexLead = RegInit((params.leadLaggWindowSize/2).U(log2Ceil(params.leadLaggWindowSize + 1).W))
+      val indexLagg = RegInit((params.leadLaggWindowSize/2).U(log2Ceil(params.leadLaggWindowSize + 1).W))
       cfar.io.indexLagg.get := indexLagg
       cfar.io.indexLead.get := indexLead
+      commonFields = commonFields :+ RegField(log2Ceil(params.leadLaggWindowSize + 1), indexLagg,
+        RegFieldDesc(name = "indexLagg", desc = "Defines index of the sorted cells inside lagging window used for the threshold calculation"))
+      commonFields = commonFields :+ RegField(log2Ceil(params.leadLaggWindowSize + 1), indexLead,
+        RegFieldDesc(name = "indexLead", desc = "Defines index of the sorted cells inside leading window used for the threshold calculation"))
     }
-  
-    val detectedPeaksList = detectedPeaksListTmp.asUInt
-    
+    //val detectedPeaksList = detectedPeaksListTmp.asUInt
+
     if (params.sendCut) {
-      out.bits.data := cfar.io.out.bits.cut.get.asUInt
-      // This should be organized differently
-      // It is not possible to save peak list in registers
-      when (cfar.io.fftBin === fftWin - 1.U) {
-        detectedPeaksListTmp.map(c => false.B)
-      }
-      .elsewhen (cfar.io.out.bits.peak) {
-        detectedPeaksListTmp(cfar.io.fftBin) := true.B
-      }
+      require(cfar.io.out.bits.threshold.getWidth + cfar.io.out.bits.cut.get.getWidth + cfar.io.fftBin.getWidth + 1 < 48)
+      out.bits.data := Cat(cfar.io.out.bits.threshold.asUInt, cfar.io.out.bits.cut.get.asUInt, cfar.io.fftBin, cfar.io.out.bits.peak)
     }
     else {
       out.bits.data    := Cat(cfar.io.out.bits.threshold.asUInt, cfar.io.fftBin, cfar.io.out.bits.peak) // output.asUInt
@@ -93,38 +153,9 @@ abstract class CFARBlock [T <: Data : Real: BinaryRepresentation, D, U, E, O, B 
     cfar.io.out.ready  := out.ready
     out.bits.last := cfar.io.lastOut
 
-    val fields = Seq(
-      RegField(log2Ceil(params.fftSize + 1), fftWin,
-        RegFieldDesc(name = "fftWin", desc = "Determines fft size")),
-      RegField(thresholdScalerWidth, thresholdScaler,
-        RegFieldDesc(name = "thresholdScaler", desc = "Threshold scaler")),
-      RegField(1, logOrLinearMode,
-        RegFieldDesc(name = "logOrLinearMode", desc = "Denotes whether input data is in log or in linear mode")),
-      RegField(divSumWidth, divSum,
-        RegFieldDesc(name = "divSum", desc = "Denotes div factor of the sliding sum when CACFAR algorithm is used")),
-      RegField(1, peakGrouping,
-        RegFieldDesc(name = "peakGrouping", desc = "Use peak grouping logic for peak detection")),
-      RegField(1, cfarAlgorithm,
-        RegFieldDesc(name = "cfarAlgorithm", desc = "Defines cfar algorithm, used only if parameter CFARAlgorithm is set to GOSCACFARType")),
-      RegField(2, cfarMode,
-        RegFieldDesc(name = "cfarMode", desc = "Defines cfar mode CA (cell averaging), GO (greatest of) or SO (smallest of)")),
-      RegField(log2Ceil(params.leadLaggWindowSize + 1), windowCells,
-        RegFieldDesc(name = "windowCells", desc = "Defines size of the leading and lagging window")),
-      RegField(log2Ceil(params.guardWindowSize + 1), guardCells,
-        RegFieldDesc(name = "guardCells", desc = "Defines size of the guard window")),
-      RegField(log2Ceil(params.leadLaggWindowSize + 1), indexLagg,
-        RegFieldDesc(name = "indexLagg", desc = "Defines index of the sorted cells inside lagging window used for the threshold calculation")),
-      RegField(log2Ceil(params.leadLaggWindowSize + 1), indexLead,
-        RegFieldDesc(name = "indexLead", desc = "Defines index of the sorted cells inside leading window used for the threshold calculation")),
-      RegField(log2Ceil(params.leadLaggWindowSize + 1) , subWindowSize,
-        RegFieldDesc(name = "subWindowSize", desc = "Defines sub-window size used in CASH algorithm"))
-      
-//      RegField.r(params.fftSize/32, detectedPeaksListTmp.asUInt,
-//         RegFieldDesc(name = "detectedPeaksList", desc = "Detected peaks list"))
-     )
     //define abract register map so it can be AXI4, Tilelink, APB, AHB
     regmap(
-      fields.zipWithIndex.map({ case (f, i) =>
+      commonFields.zipWithIndex.map({ case (f, i) =>
           i * beatBytes -> Seq(f)
       }): _*
     )
@@ -143,10 +174,11 @@ object CFARDspBlock extends App
     protoScaler = FixedPoint(16.W, 0.BP),
     leadLaggWindowSize = 64,
     guardWindowSize = 4,
+    logOrLinReg = true,
     fftSize = 1024,
     sendCut = false,
-    minSubWindowSize = None,
-    includeCASH = false,
+    minSubWindowSize = Some(8),
+    includeCASH = true,
     CFARAlgorithm = CACFARType
   )
 
