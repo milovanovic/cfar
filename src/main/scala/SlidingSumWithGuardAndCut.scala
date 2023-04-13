39060ce02ec520b6f36d6946ea298fd3b0fb0490
Vladimir Milovanović <barun@eecs.berkeley.edu>
2023-04-13 17:08:58 +0200

    Fixes and additions

--- src/main/scala/SlidingSumWithGuardAndCut.scala
+++ src/main/scala/SlidingSumWithGuardAndCut.scala
@@ -21,33 +21,42 @@ class SlidingSumWithGuardAndCut[T <: Data : Real : BinaryRepresentation](val par
 
   val retiming = if (params.retiming) 1 else 0
   //val depthOfQueue = if (params.retiming) 1 + thresholdPip else thresholdPip
-  val latency = io.depthRunTime.getOrElse(params.depth.U) +& (io.guardRunTime.getOrElse(params.guardCells.U) >> 2.U) +& 1.U
+  val latency = io.depthRunTime.getOrElse(params.depth.U) +& (io.guardRunTime.getOrElse(params.guardCells.U) >> 1.U) +& 1.U
 
-  assert(io.windowRunTime.getOrElse(params.testWindowSize.U) > 2.U * io.depthRunTime.getOrElse(params.depth.U) + io.guardRunTime.getOrElse(params.guardCells.U) + 1.U, "Windowing size shall be larger than total number of shifting cells inside sliding sum")
-  assert(io.guardRunTime.getOrElse(params.guardCells.U) > 0.U, "Number of guard cells should be greater than 0")
+  //assert(io.windowRunTime.getOrElse(params.testWindowSize.U) > 2.U * io.depthRunTime.getOrElse(params.depth.U) + 00io.guardRunTime.getOrElse(params.guardCells.U) + 1.U, "Windowing size shall be larger than total number of shifting cells inside sliding sum")
+  //assert(io.guardRunTime.getOrElse(params.guardCells.U) > 0.U, "Number of guard cells should be greater than 0")
 
-  val sumT: T = (io.in.bits * log2Ceil(params.depth)).cloneType // no overflow
+  val sumT: T = params.protoOut ///(io.in.bits * log2Ceil(params.depth)).cloneType // no overflow
   val sumlagg = RegInit(t = sumT, init = 0.U.asTypeOf(sumT))
   val sumlead = RegInit(t = sumT, init = 0.U.asTypeOf(sumT))
 
-  val slidingWin1 = Module(new AdjustableShiftRegisterStream(params.protoIn, params.depth))
+  val slidingWin1 = Module(new AdjustableShiftRegisterStream(params.protoIn, params.depth, false, false, false))
   slidingWin1.io.in <> io.in
   slidingWin1.io.depth := io.depthRunTime.getOrElse(params.depth.U)
   slidingWin1.io.lastIn := io.lastIn
 
-  val lastOut = ShiftRegister(slidingWin1.io.lastOut, params.guardCells/2, true.B) // treba da bude adjustable shift register, gde mu je depth run-time konfigurabilan
-  val guardCells = Module(new AdjustableShiftRegisterStream(params.protoIn, params.guardCells))
+  // not optimal solution but while guardCells size normally is not large then this is ok
+  val runTimeGuardCells = 3 to params.guardCells by 2
+  val lastOuts = Wire(Vec(runTimeGuardCells.size, Bool()))
+  // Remove shift-register with highest depth
+  lastOuts.zipWithIndex.map { case (lastOut, index) => lastOut := ShiftRegister(slidingWin1.io.lastOut, (index*2 + 3)/2 + 1, io.out.fire) }
+
+  val lastOut = if (params.runTimeGuard)
+                  Mux(io.guardRunTime.get =/= params.guardCells.U, lastOuts(io.guardRunTime.get >> 1.U - 1.U),  ShiftRegister(slidingWin1.io.lastOut, params.guardCells/2 + 1, io.out.fire))
+                else
+                  ShiftRegister(slidingWin1.io.lastOut, params.guardCells/2 + 1, io.out.fire) // +2 or + 1 - when no adders are included
+  val guardCells = Module(new AdjustableShiftRegisterStream(params.protoIn, params.guardCells, true, false, false))
   guardCells.io.in <> slidingWin1.io.out
   guardCells.io.depth := io.guardRunTime.getOrElse(params.guardCells.U)
   guardCells.io.lastIn := slidingWin1.io.lastOut
 
-  val slidingWin2 = Module(new AdjustableShiftRegisterStream(params.protoIn, params.depth))
+  val slidingWin2 = Module(new AdjustableShiftRegisterStream(params.protoIn, params.depth, false, false, false))
   slidingWin2.io.in <> guardCells.io.out
   slidingWin2.io.depth := io.depthRunTime.getOrElse(params.depth.U)
   slidingWin2.io.lastIn := guardCells.io.lastOut
   slidingWin2.io.out.ready := io.out.ready
 
-  slidingWin1.io.out.ready := guardCells.io.in.ready // this is a question, maybe it will not be bidirectional connection
+  slidingWin1.io.out.ready := guardCells.io.in.ready
   guardCells.io.out.ready := slidingWin2.io.in.ready
 
   when (io.in.fire) {
@@ -77,7 +86,7 @@ class SlidingSumWithGuardAndCut[T <: Data : Real : BinaryRepresentation](val par
                                                 resetData = false.B,
                                                 reset = io.lastOut,
                                                 en = true.B)
-  when (io.lastOut) {
+  when (io.lastOut && io.out.fire()) {
     lastCut := true.B
     flushing := false.B
     initialInDone := false.B
@@ -99,6 +108,14 @@ class SlidingSumWithGuardAndCut[T <: Data : Real : BinaryRepresentation](val par
       sumlagg := sumlagg + slidingWin1.io.in.bits
     }
   }
+  val condition = WireInit(false.B)
+  condition := cntOut > (io.windowRunTime.getOrElse(params.testWindowSize.U) - (io.depthRunTime.getOrElse(params.depth.U)) - (io.guardRunTime.getOrElse(params.guardCells.U) >> 1.U) - 2.U)
+
+  when (condition && slidingWin1.io.out.fire) {
+    sumlagg := sumlagg - slidingWin1.io.out.bits
+  }
+
+  dontTouch(condition)
   dontTouch(sumlagg)
 
   when (lastCut) {
@@ -133,12 +150,12 @@ class SlidingSumWithGuardAndCut[T <: Data : Real : BinaryRepresentation](val par
     io.out.valid := initialInDone && io.in.fire || flushing
     if (params.sendMiddle)
       io.out.bits.middleCell.get := cutDelayed
-    io.in.ready := ~initialInDone || io.out.ready && ~flushing
+    //io.in.ready := ~initialInDone || io.out.ready && ~flushing
     io.lastOut := lastOut
   }
   else {
     val queueOut = Module(new Queue((io.out.bits.cloneType), params.numAddPipes + 1, flow = true))
-    io.in.ready := ~initialInDone || io.out.ready && ~flushingDelayed
+    //io.in.ready := ~initialInDone || io.out.ready && ~flushingDelayed
     queueOut.io.enq.valid := ShiftRegister(initialInDone && io.in.fire, params.numAddPipes, en = true.B) || (flushingDelayed && ShiftRegister(io.out.ready, params.numAddPipes, en = true.B))
 
     if (params.sendMiddle)
